6f7ae05441ce05afb33cc3e44c69891d35052d2f
Richies Huynh <89647964+BurningIcicles@users.noreply.github.com>
2024-11-10 22:30:00 -0800

    changes

--- Assets/Scripts/Player/EnemyCollision.cs
+++ Assets/Scripts/Player/EnemyCollision.cs
@@ -17,7 +17,7 @@ public class EnemyCollision : MonoBehaviour
         animator = GetComponent<Animator>();
         soundController = GameObject.FindGameObjectWithTag("SoundController").GetComponent<SoundController>();
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
